3adcab565b2f6825864cece7405e80c2b64124f1
RendraDJ <rendra.djunaedi@etu.umontpellier.fr>
2024-12-05 16:16:29 +0100

    test

--- scenes/menus/main_menu/mainMenu_scripts/main_menu.gd
+++ scenes/menus/main_menu/mainMenu_scripts/main_menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 func _on_start_button_pressed() -> void:
 	pass # Replace with function body.
-	get_tree().change_scene_to_file("res://scenes/character_selection/character_selection.tscn")
+	get_tree().change_scene_to_file("res://scenes/map/map2.tscn")
 
 func _on_options_button_pressed() -> void:
 	pass # Replace with function body.
