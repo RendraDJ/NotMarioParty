9811db8ca2365dd2ca84b98789307ce576c7cb73
RendraDJ <114173783+RendraDJ@users.noreply.github.com>
2024-12-14 21:10:09 +0100

    Delete gamev2/not_gto! directory

--- gamev2/not_gto!/scripts/candy_1.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-func _ready() -> void:
-	connect("body_entered", Callable(self, "_on_body_entered"))
-
-func _on_body_entered(body: Node) -> void:
-	if body is AgentEleve1:
-		queue_free()
--- gamev2/not_gto!/state_label.gd
+++
@@ -1,6 +0,0 @@
-extends Label
-
-@export var target: Node
-
-func _process(_delta):
-	text = target.State
